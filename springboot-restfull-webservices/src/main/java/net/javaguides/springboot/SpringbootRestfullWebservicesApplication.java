63b4804949f04e7a5fa16e4cb8d6f9a012ef9624
shubhamithub <shubhampandeyitsoftware2022@gmail.com>
2023-11-20 17:06:14 +0530

    updating restfull

---
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullWebservicesApplication.class, args);
+	}
+
+}
